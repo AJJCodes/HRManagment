b9b133c43ce3238e94eee5e93954dba31eb75838
Fran <Fran@DESKTOP-SNTT26F>
2024-07-04 00:52:49 -0600

    Grega al de roles

---
+++ Logica/Seguridad/VistaUsuario_LN.cs
@@ -0,0 +1,50 @@
+﻿using Datos.BaseDatos;
+using Modelo.Nomina;
+using Modelo.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Seguridad
+{
+    public class VistaUsuario_LN
+    {
+        private readonly Contexto bd;
+        public VistaUsuario_LN()
+        {
+            bd = new Contexto();
+        }
+
+        #region Consulta
+        public bool ProporcionarListaUsuario(ref List<RegistroVistaUsuario> ListaUsuario, out string? errorMessage)
+        {
+            try
+            {
+                // Utilizar LINQ para seleccionar los campos necesarios y mapearlos a Roles_VM
+                ListaUsuario = (from us in bd.Usuario
+                                join R in bd.Roles on us.IdRol equals R.IdRol
+                                join Co in bd.Colaboradores on us.IdUsuario equals Co.IdUsuario
+                                join C in bd.Contrato on Co.IdColaborador equals C.IdColaborador
+                                join DL in bd.DatosLaborales on C.IdContrato equals DL.IdContrato
+                               select new RegistroVistaUsuario
+                               {
+                                   IdUsuario = us.IdUsuario,
+                                   NombreColaborador = Co.NombresColaborador + " " + Co.ApellidosColaborador,
+                                   CodigoColaborador = DL.CodigoColaborador,
+                                   NombreUsuario = us.NombreUsuario,
+                                   NombreRol = R.NombreRol
+                               }).ToList();
+                errorMessage = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
--- Web/Controllers/Seguridad/RolesController.cs
+++ Web/Controllers/Seguridad/RolesController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Logica.Seguridad;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers.Seguridad
 {
     public class RolesController : Controller
     {
+        private readonly VistaUsuario_LN _roles_LN;
+
+        public RolesController(IHttpContextAccessor httpContextAccessor)
+        {
+            _roles_LN = new VistaUsuario_LN();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ObtenerListaSeguridad()
+        {
+            List<RegistroVistaUsuario> listaSeguridad = new List<RegistroVistaUsuario>();
+            if (_roles_LN.ProporcionarListaUsuario(ref listaSeguridad, out string? errorMessage))
+            {
+                return Json(new { data = listaSeguridad });
+            }
+            else
+            {
+                return Json(new { error = errorMessage });
+            }
         }
     }
 }
