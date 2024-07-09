4bc605b5df26891ef6207bd4aebd2ef4d89e9680
Anthony <anthonyj300@yahoo.com>
2024-07-09 00:52:59 -0600

    Actualizacion de cadena de conexion

--- Datos/BaseDatos/Contexto.cs
+++ Datos/BaseDatos/Contexto.cs
@@ -37,7 +37,7 @@ public partial class Contexto : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=sql8006.site4now.net; Database=db_aaa279_hrmanagment; User ID=db_aaa279_hrmanagment_admin; Password='Dario#$%23';");
+        => optionsBuilder.UseSqlServer("Server=sql8005.site4now.net; Database=db_aaa279_hrmanag; User ID=db_aaa279_hrmanag_admin; Password='Dario#$%23';");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
